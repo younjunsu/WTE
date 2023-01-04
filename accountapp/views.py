fb49d6de49f5b24ddde74e51ac437120090fb111
younjunsu <junsu_youn@naver.com>
2023-01-04 10:54:42 +0900

    test code

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,7 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
-
 def hello_world(request):
     return render(request, 'accountapp/hello_world.html')

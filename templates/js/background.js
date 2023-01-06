60bb1d197b414bcc89673b7559c51480f870ae50
younjunsu <junsu_youn@naver.com>
2023-01-06 15:22:59 +0900

    test code

--- templates/js/background.js
+++
@@ -1,9 +0,0 @@
-const images = [
-    "0.png", "1.png", "2.png"
-];
-
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-const bgImage = document.createElement("img");
-
-bgImage.src = `img/${chosenImage}`;
-document.body.appendChild(bgImage);
